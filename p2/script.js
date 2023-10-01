3e0a579c306e87fd0775e280ac084b07aa8f174f
Luizx11978 <luizzz.z@outlook.com>
2023-10-01 14:47:57 -0300

    correção de erros

---
+++ p2/script.js
@@ -0,0 +1,36 @@
+const caixa1 = document.querySelector("#caixa1")
+const caixa2 = document.querySelector("#caixa2")
+const btn1 = document.querySelector("#btn")
+const todosCursos=[...document.querySelectorAll(".curso")]
+
+todosCursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const curso = evt.target
+        curso.classList.toggle("selecionado")
+    })
+})
+
+const classDelete = (el)=>{
+    el.classList.remove("selecionado")
+}
+btn1.addEventListener("click",()=>{
+    const cursoSelecionados = [...document.querySelectorAll(".selecionado")]
+    const selecionados = document.querySelector(".selecionado")
+    if(caixa1.contains(selecionados)){
+        cursoSelecionados.map((el)=>{
+            caixa2.appendChild(el)
+            classDelete(el)
+        })
+    }
+    else{
+        cursoSelecionados.map((el)=>{   
+            caixa1.appendChild(el)
+            classDelete(el)
+        })
+    }
+})
+
+
+
+
+
